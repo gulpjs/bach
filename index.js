aef13e081b3b2c4af62a258100c352f581a9b779
Blaine Bublitz <blaine@iceddev.com>
2014-06-21 17:47:31 -0700

    reorganize, add timing

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-var series = require('./series');
-var parallel = require('./parallel');
-var settleSeries = require('./settleSeries');
-var settleParallel = require('./settleParallel');
+'use strict';
+
+var asyncDone = require('async-done');
+
+var createSeries = require('./lib/createSeries');
+var createParallel = require('./lib/createParallel');
 
 module.exports = {
-  series: series,
-  parallel: parallel,
-  settleSeries: settleSeries,
-  settleParallel: settleParallel,
+  series: createSeries(asyncDone),
+  parallel: createParallel(asyncDone)
 };
--- settle/series.js
+++ settle/series.js
@@ -4,7 +4,7 @@ var _ = require('lodash');
 var async = require('async');
 var asyncSettle = require('async-settle');
 
-var onSettled = require('./lib/onSettled');
+var onSettled = require('../lib/onSettled');
 
 function buildParallel(){
   var args = _.flatten(arguments);
