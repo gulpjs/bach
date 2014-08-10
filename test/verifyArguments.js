d1a920cbb18808eea5609412189003666ddbd2f2
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2014-08-10 18:13:11 +0200

    verify arguments to serries / parallel functions
    Fixes #4

---
+++ test/verifyArguments.js
@@ -0,0 +1,32 @@
+'use strict';
+
+var test = require('tap').test;
+
+var verifyArguments = require('../lib/verifyArguments');
+
+function validArg() {
+}
+
+test('should act as pass-through for a valid set of arguments', function(t){
+  var args = [validArg, validArg];
+  t.deepEqual(verifyArguments(args), args);
+  t.end();
+});
+
+test('should throw descriptive error message on invalid argument', function(t){
+  t.throws(function(){
+    verifyArguments([validArg, 'invalid', validArg]);
+  }, {
+    name: 'AssertionError', message: 'Only functions can be combined, got string for argument 1'
+  }, 'should throw AssertionError');
+  t.end();
+});
+
+test('should throw descriptive error message on when no arguments provided', function(t){
+  t.throws(function(){
+    verifyArguments([]);
+  }, {
+    name: 'AssertionError', message: 'A set of functions to combine is mandatory'
+  }, 'should throw AssertionError');
+  t.end();
+});
