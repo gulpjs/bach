54b0a6e2edcf18840427d57acbe58239d5304373
Blaine Bublitz <blaine.bublitz@gmail.com>
2014-08-17 15:29:43 -0700

    Breaking: Remove timing methods & switch to now-and-later for series/parallel

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
 'use strict';
 
-var asyncDone = require('async-done');
-
-var createSeries = require('./lib/createSeries');
-var createParallel = require('./lib/createParallel');
-
 module.exports = {
-  series: createSeries(asyncDone),
-  parallel: createParallel(asyncDone)
+  series: require('./lib/series'),
+  parallel: require('./lib/parallel')
 };
