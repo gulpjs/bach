1e4f185d47c6f717b650b279b1965faeb8e7f7c1
sttk <sttk.xslet@gmail.com>
2022-05-29 10:22:44 +0900

    update: drops arr-filter, array-map, array-each, array-initial, and array-last

--- test/onSettled.js
+++ test/onSettled.js
@@ -9,6 +9,11 @@ var errors = [
   { state: 'error', value: new Error('Error 2') },
 ];
 
+var successes = [
+  { state: 'success', value: 'Success 1' },
+  { state: 'success', value: 'Success 2' },
+];
+
 describe('onSettled', function() {
 
   it('should group all errors', function(done) {
@@ -35,5 +40,21 @@ describe('onSettled', function() {
   it('should handle non-functions as callbacks', function(done) {
     onSettled('not a function')(null, errors);
     done();
+  });
+
+  it('should pass `value` props of results if no error', function(done) {
+    onSettled(function(errs, results) {
+      expect(errs).toEqual(null);
+      expect(results).toEqual(['Success 1', 'Success 2']);
+      done();
+    })(null, successes);
+  });
+
+  it('should handle the case that result is nullish', function(done) {
+    onSettled(function(errs, results) {
+      expect(errs).toEqual(null);
+      expect(results).toEqual(null);
+      done();
+    })(null, null);
   });
 });
